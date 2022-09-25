7c991ef97b1afc2479967b2ab18133be4169a201
NguyenWhamyTech <110946003+NguyenWhamyTech@users.noreply.github.com>
2022-09-25 10:25:50 +0700

    update 25/9 fix url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./Component/Silder/header/Header";
+import Header from "./components/header/Header";
 import Home from "./container/Home";
 
 function App() {
